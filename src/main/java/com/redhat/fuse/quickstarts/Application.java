b47ddf5fb431a3f17ec93e87da4749ccaba998d1
Luigi De Masi <ldemasi@redhat.com>
2023-04-06 17:49:27 +0200

    ENTESB-20969: [Fuse On Openshift] QS Spring-Boot Camel XA throws bad SQL grammar exception on PostGresSQL connection

--- src/main/java/com/redhat/fuse/quickstarts/Application.java
+++ src/main/java/com/redhat/fuse/quickstarts/Application.java
@@ -15,29 +15,12 @@
  */
 package com.redhat.fuse.quickstarts;
 
-import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.jms.ConnectionFactory;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-    @Bean(name = "jms-component")
-    public JmsComponent jmsComponent(ConnectionFactory xaJmsConnectionFactory, PlatformTransactionManager jtaTransactionManager) {
-	    JmsComponent jms = new JmsComponent();
-	    jms.setConnectionFactory(xaJmsConnectionFactory);
-	    jms.setTransactionManager(jtaTransactionManager);
-	    jms.setTransacted(true);
-
-	    return jms;
-    }
-
 }
